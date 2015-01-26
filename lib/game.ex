11420e151e139cdb4a1759fffb984fff73223074
Otto <ottolin@gmail.com>
2015-01-27 01:07:43 +0800

    adding who_to_move call in game

--- lib/game.ex
+++ lib/game.ex
@@ -25,6 +25,10 @@ defmodule Game do
     GenServer.call(game, :info)
   end
 
+  def who_to_move(game) do
+    GenServer.call(game, :who_to_move)
+  end
+
   def play(game) do
     GenServer.call(game, :play)
   end
@@ -35,11 +39,21 @@ defmodule Game do
   end
 
   def handle_call({:join, player}, _from, gInfo) do
-    {:reply, :ok, %{gInfo | players: [player | gInfo.players]}}
+    cond do
+      length(gInfo.players) < 4 ->
+        newInfo = %{gInfo | players: [player | gInfo.players]}
+        {:reply, {:ok, length(newInfo.players)}, newInfo}
+      true ->
+        {:reply, :err_game_full, gInfo}
+    end
   end
 
   def handle_call(:info, _from, gInfo) do
     {:reply, gInfo, gInfo}
+  end
+
+  def handle_call(:who_to_move, _from, gInfo) do
+    {:reply, {gInfo.curPlayerId, gInfo.players |> Enum.at(gInfo.curPlayerId)}, gInfo}
   end
 
   def handle_call(:play, _from, gInfo) do
